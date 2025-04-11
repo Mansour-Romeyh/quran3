2ffc46ce0ed45c68dd805de4d48076dc66c1b30f
MansourRomeyh <mansourromeyh@192.168.1.10>
2025-04-11 13:12:27 +0200

    last update

--- lib/controller/splashcontroller.dart
+++ lib/controller/splashcontroller.dart
@@ -1,15 +1,19 @@
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 import 'package:quran3/core/constant/approutes.dart';
 
+
 class Splashcontroller extends GetxController {
+   static AudioPlayer audioPlayer = AudioPlayer();
   gotOnpording() {
     Get.offNamed(Approutes.onpording);
   }
 
   @override
   void onInit() {
-    Future.delayed(Duration(seconds: 4), () {
+    audioPlayer.play(AssetSource('audio/mensh.m4a'));
+    Future.delayed(Duration(seconds: 6), () {
       gotOnpording();
     });
     super.onInit();
